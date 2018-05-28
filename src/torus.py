0f59cb606ef5358bb2557a6bb8b9ee212a8fb203
Prannay Khosla <prannay.khosla@gmail.com>
2018-05-28 14:07:51 +0200

    Added ellipse, torus.

--- src/torus.py
+++ src/torus.py
@@ -10,17 +10,11 @@ import numpy as np
 import tensorflow as tf
 
 class Sphere(object):
-    def __init__(self, r=1, R1=5, R2=5, theta_base=0, phi_base=0, gamma_base=0) : # three degrees of rotation
+    def __init__(self, r=1, R=5, theta_base=0) :
         self.theta_range = np.pi
         self.phi_range = 2*np.pi
         self.r = r
-        self.R1 = R1
-        self.R2 = R2
-        self.z_mat = np.array([[np.cos(theta_base), -np.sin(theta_base),0],[np.sin(theta_base), np.cos(theta_base),0],[0 , 0, 1]]).astype(np.float32)
-        self.x_mat = np.array([[1,0,0], [0, np.cos(phi_base), -np.sin(phi_base)],[0, np.sin(phi_base), np.cos(phi_base)]])
-        self.y_mat = np.array([[np.cos(gamma_base),0,np.sin(gamma_base)], [0, 1,0], [-np.sin(phi_base), 0, np.cos(phi_base)]])
-        self.mat1 = np.matmul(self.x_mat, self.z_mat)
-        self.mat1 = np.matmul(self.y_mat, self.mat1)
+        self.R = R
 
     def _single_point_generator(self):
         x = np.random.uniform(0, self.theta_range), np.random.uniform(0, self.phi_range)
@@ -28,7 +22,7 @@ class Sphere(object):
 
     def _multi_point_generator(self, num):
         x = zip(np.random.uniform(0, self.theta_range, num), np.random.uniform(0, self.phi_range, num))
-        return np.array(map(lambda x : np.matmul(self.mat1, np.array([i for i in reversed([self.r*np.cos(x[0]), (self.R1)*np.sin(x[0])*np.sin(x[1]), (self.R2*np.sin(x[0])*np.cos(x[1]))])])),list(set(x)))) # ensure there are no double matches
+        return np.array(map(lambda x : [i for i in reversed([self.r*np.cos(x[0]), (self.R + self.r*np.sin(x[0])*np.sin(x[1])), (self.R + self.r*np.sin(x[0])*np.cos(x[1]))])],list(set(x)))) # ensure there are no double matches
 
     def _euclidean_metric(self, p1, p2, h):
         return np.sqrt(np.sum(np.square(p1-p2))/h**2)
@@ -37,18 +31,12 @@ class Sphere(object):
         return (np.dot(p1, p2) + 1) / (2*h)
 
     def _sphere_metric(self, p1, p2, h):
-        p1 = np.matmul(np.linalg.inv(self.y_mat),p1)
-        p1 = np.matmul(np.linalg.inv(self.x_mat),p1)
-        p1 = np.matmul(np.linalg.inv(self.z_mat),p1)
-        p2 = np.matmul(np.linalg.inv(self.y_mat),p2)
-        p2 = np.matmul(np.linalg.inv(self.x_mat),p2)
-        p2 = np.matmul(np.linalg.inv(self.z_mat),p2)
-        theta1 = np.arctan((np.sqrt((p1[0]/self.R2)**2 + (p1[1]/ self.R1)**2) ) / p1[2])
-        phi1 = np.arctan((self.R2*p1[1])/(self.R1*p1[0]))
+        theta1 = np.arctan((np.sqrt(p1[0]**2 + p1[1]**2) - self.R) / p1[2])
+        phi1 = np.arctan(p1[1]/p1[0])
         if(p1[0] < 0) :
             phi1 = np.pi + phi1
-        theta2 = np.arctan((np.sqrt((p2[0]/self.R2)**2 + (p2[1]/ self.R1)**2) ) / p2[2])
-        phi2 = np.arctan((self.R2*p2[1])/(self.R1*p2[0]))
+        theta2 = np.arctan((np.sqrt(p2[0]**2 + p2[1]**2) - self.R) / p2[2])
+        phi2 = np.arctan(p2[1]/p2[0])
         if(p2[0] < 0) :
             phi2 = np.pi + phi2
         if (h == 1.1):
@@ -90,7 +78,7 @@ tf.app.flags.DEFINE_string("metric", "sphere", "Metric for computation")
 flags = tf.app.flags.FLAGS
 
 if __name__ == "__main__" :
-    sphere = Sphere(r=1, R1=1, R2=4)
+    sphere = Sphere()
     zero = np.zeros(3)
     list_pts = sphere._multi_point_generator(50)
     for h in [0.2, 0.5, 0.8, 1.0, 1.5, 2.0, 2.5]:
